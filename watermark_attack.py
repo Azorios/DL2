3264c91c53582a2e6e22b0c77a772f0e8b566b3e
Azorios <rosaalani99@gmail.com>
2023-08-18 13:35:56 +0200

    Add files via upload

---
+++ watermark_attack.py
@@ -0,0 +1,83 @@
+from diffusers import AutoencoderKL
+import os
+import torch
+import numpy as np
+from PIL import Image
+from einops import rearrange, repeat
+from ldm.util import instantiate_from_config
+from omegaconf import OmegaConf
+from torchvision import transforms
+
+def load_model_from_config(config, ckpt):
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    model.load_state_dict(sd, strict=False)
+    model.cuda()
+    model.eval()
+    return model
+
+def load_img(path):
+    image = Image.open(path).convert("RGB")
+    w, h = image.size
+    w, h = map(lambda x: x - x % 64, (w, h))  # resize to integer multiple of 32
+    image = image.resize((w, h), resample=Image.LANCZOS)
+    image = np.array(image).astype(np.float32) / 255.0
+    image = image[None].transpose(0, 3, 1, 2)
+    image = torch.from_numpy(image)
+    return 2.*image - 1.
+
+def main():
+    input_dir = '../optimize_image/watermark_data/val/watermarked/0'
+    output_dir = './output/imgs_cocoval/watermarked_removed_kandinsky'
+    os.makedirs(output_dir, exist_ok=True)
+    config_path = "configs/stable-diffusion/v1-inference.yaml"
+    ckpt_path = "v1-5-pruned-emaonly.ckpt"
+
+    config = OmegaConf.load(config_path)
+    model = load_model_from_config(config, ckpt_path)
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = model.to(device)
+
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            img_path = os.path.join(input_dir, filename)
+            init_image = load_img(img_path).to(device)
+            init_latent = model.get_first_stage_encoding(model.encode_first_stage(init_image))  # move to latent space
+            output_image = model.decode_first_stage(init_latent)
+            output_image = torch.clamp((output_image + 1.0) / 2.0, min=0.0, max=1.0)
+            output_image = 255. * rearrange(output_image.cpu().numpy(), '1 c h w -> h w c')
+            Image.fromarray(output_image.astype(np.uint8)).save(os.path.join(output_dir, filename))
+
+def apply_auto(filname, model):
+    print(filname)
+    init_image = load_img(os.path.join(input_dir, filename)).to(device)
+
+
+    encoding = model.encode(init_image).latent_dist.sample().to(device)
+
+    decoding = model.decode(encoding).sample
+
+    # convert back to cpu before converting to PIL image
+
+    output_image = decoding
+    # Normalize Tensor transform values to be between 0 and 1
+    output_image = (output_image+1)/2
+    output_image = output_image.cpu().detach().numpy()
+    output_image = 255. * rearrange(output_image, '1 c h w -> h w c')
+    Image.fromarray(output_image.astype(np.uint8)).save(os.path.join(output_dir, filename))
+
+if __name__ == "__main__":
+    model = AutoencoderKL.from_pretrained("kandinsky-community/kandinsky-2-2-prior", subfolder="prior",double_z=False)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = model.to(device)
+
+    input_dir = '../optimize_image/watermark_data/val/watermarked/0'
+    output_dir = './out_kandinsky'
+    for filename in os.listdir(input_dir):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            apply_auto(filename,model)
+
+
+
